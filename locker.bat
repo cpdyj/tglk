e9dd53482256f6c0259bd6a2275b86afb9032b4b
iseki <admin@iseki.space>
2021-02-22 18:59:20 +0800

    add batch file

---
+++ locker.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Edit the bat file before execute!
+pause
+exit
+
+set botkey=**********:===================================
+set admin_uid=****
+set https_proxy=http://HOST:PORT
+taskkill /im tglk.exe /f
+taskkill /im tglk.exe /f
+.\tglk.exe 
---
+++ locker.vbs
@@ -0,0 +1,3 @@
+dim s
+set s = CreateObject("Wscript.Shell")
+s.Run "locker.bat", 0, True
